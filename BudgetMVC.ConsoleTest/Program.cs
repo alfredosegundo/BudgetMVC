8e397c1af6753ce3e06cf0931941306b02c2ead0
Alfredo Cavalcanti Segundo <alfredocavalcanti@gmail.com>
2012-12-16 14:30:46 -0300

    Installing MvcScaffolding
    Added console app to deal more eficietily with entity framework models

---
+++ BudgetMVC.ConsoleTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BudgetMVC.Model.EntityFramework;
+using BudgetMVC.Model.Entity;
+
+namespace BudgetMVC.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using( var db = new BudgetContext())
+            {
+                db.Expenses.Add(new Expense { Description = DateTime.Now.ToLongDateString(), Value = DateTime.Now.Second * 0.38});
+                db.SaveChanges();
+                var query = from expense in db.Expenses
+                            orderby expense.CreationDate
+                            select expense;
+
+                Console.WriteLine("Database: " + db.Database.Connection.ConnectionString);
+                Console.WriteLine("All expenses in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine("Date: " + item.CreationDate);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
